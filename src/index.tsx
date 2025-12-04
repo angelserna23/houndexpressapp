91f80b094a3658f49a1c00e7ecd5df369d0f92a6
Angel Serna <andres.hdzserna@gmail.com>
2025-12-04 14:35:09 -0600

    Modifications for index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,21 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./assets/styles/global.scss";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store";
 
 const rootElement = document.getElementById("root") as HTMLElement;
-
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
-
